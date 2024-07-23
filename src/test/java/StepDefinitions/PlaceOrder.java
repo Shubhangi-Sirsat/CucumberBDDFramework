ee5e407b581cc61347c0668398a2b101205ab8ad
Shubhangi <sirsatshubhangi19@gmail.com>
2024-07-23 14:03:14 +0530

    place order updated

---
+++ src/test/java/StepDefinitions/PlaceOrder.java
@@ -0,0 +1,34 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PlaceOrder {
+	
+	@Given("User login to homepage")
+	public void user_login_to_homepage() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User selcts one product")
+	public void user_selcts_one_product() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("User add product to cart")
+	public void user_add_product_to_cart() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User the place order")
+	public void user_the_place_order() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+
+}
